3e70c46f72171ade370234feb62f1ea9c2517a3a
Charles Seaton <seatonct@gmail.com>
2023-12-08 16:02:55 -0600

    started on My Journal Component

--- src/managers/EntryManager.jsx
+++ src/managers/EntryManager.jsx
@@ -9,3 +9,15 @@ export const getAllEntries = async () => {
   const allEntries = res.json();
   return allEntries;
 };
+
+export const getEntriesByUserId = async () => {
+  const res = await fetch(`http://localhost:8000/entries?userId=${userId}`, {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  const userEntries = res.json();
+  return userEntries;
+};
