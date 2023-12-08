761519c8128dc9ce572128a02fc0cebabd4465ad
Charles Seaton <seatonct@gmail.com>
2023-12-08 12:03:29 -0600

    completed basic Home component

--- src/managers/EntryManager.jsx
+++ src/managers/EntryManager.jsx
@@ -1,7 +1,9 @@
-export const getAllEntries = async ({ token }) => {
+export const getAllEntries = async () => {
   const res = await fetch("http://localhost:8000/entries", {
     headers: {
-      Authorization: `Token ${token}`,
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
   });
   const allEntries = res.json();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "gin-clear": "#FFFFFF",
+        "white-wine": "#FFFEDC",
+        "pale-straw": "#FEEE98",
+        "pale-gold": "#FAEA77",
+        "jonquil-ripe-corn": "#FBE166",
+        "yellow-gold": "#F8DC55",
+        "old-gold": "#F8D948",
+        "amber-whiskey": "#F7CF49",
+        "deep-gold": "#F5C94D",
+        "amontillado-sherry": "#F7C23B",
+        "deep-copper": "#F4BD33",
+        "burnished-whiskey": "#ECAD06",
+        "chestnut-oloroso-sherry": "#E79E12",
+        "russet-muscat": "#E69500",
+        "tawny-whiskey": "#DD7315",
+        "auburn-polished-mahogany": "#E26529",
+        "mahogany-henna-notes": "#CC502E",
+        "burnt-umber": "#AE3029",
+        "old-oak": "#9F221D",
+        "brown-sherry": "#732D1D",
+        "treacle-whiskey": "#421D0B",
+      },
+    },
   },
   plugins: [],
 };
